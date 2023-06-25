3e81bdf3645bc1326b574f5c009aca6b25ecd76d
abcmehedi5 <abcmehedi5@gmail.com>
2023-06-25 23:29:45 +0600

    project api

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-require('dotenv').config()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+const project = require("./projects.json");
+const cors = require("cors");
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
+// data base connection
 
-
-// data base connection 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mbjz2.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,11 +18,11 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-const db = client.db('protfolio')
-const projectscollection = db.collection('projects')
+const db = client.db("protfolio");
+const projectscollection = db.collection("projects");
 
 async function run() {
   try {
@@ -31,37 +30,26 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
+    // api start---------------------
 
+    app.post("/project", (req, res) => {
+      console.log(req.body);
+    });
 
+    app.get("/project", (req, res) => {
+      res.send(project);
+    });
+    app.get("/project/:id", (req, res) => {
+      const id = req.params.id;
+      const query = project.find((project) => project._id == id);
+      res.send(query);
+    });
 
-// api start---------------------
-
-
-app.post('/project' , (req , res) =>{
-    console.log(req.body)
-
-    
-})
-
-app.get('/project' , (req , res) =>{
-
-})
-
-
-
-
-
-
-
-// api end-----------------------
-
-
-
-
-
-
+    // api end-----------------------
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -69,29 +57,10 @@ app.get('/project' , (req , res) =>{
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Protfolio app listening on port ${port}`)
-})
+  console.log(`Protfolio app listening on port ${port}`);
+});
